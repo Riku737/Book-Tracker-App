d1213f758b31ddae8835cff9814a4cf0caa6f295
Riki <rmcal024@uottawa.ca>
2026-07-09 15:36:49 -0400

    Added link to book cover image

--- src/pages/Bookshelf.jsx
+++ src/pages/Bookshelf.jsx
@@ -16,7 +16,7 @@ export default function Bookshelf() {
 
             <div className="row g-4">
                 {books.map((book) => (
-                    <BookshelfBookCard book={book} key={book.id} />
+                    <BookshelfBookCard book={book} key={book.id}/>
                 ))}
             </div>
         </>
