4d118458203423eed850f0acc0ac4d63e7dd044b
Riki <rmcal024@uottawa.ca>
2026-07-09 15:33:36 -0400

    Added carousel of covers to book page

---
+++ src/components/BookCoverCarousel.jsx
@@ -0,0 +1,47 @@
+import "../css/book.css";
+
+export default function BookCoverCarousel({book}) {
+    return(
+        <>
+            <div className="bg-light ratio ratio-1x1 rounded">
+                {(book.covers) ? (
+                    <div id="carouselExample" className="carousel slide">
+
+                        <div className="carousel-inner ratio d-flex justify-content-center">
+
+                            {book.covers.map((cover, index) => (
+                                <img
+                                    key={index}
+                                    src={`https://covers.openlibrary.org/b/id/${cover}-L.jpg`}
+                                    alt={book.title}
+                                    className={`carousel-item ${index === 0 ? "active" : ""} object-fit-contain p-4`}
+                                />
+                            ))}
+
+                        </div>
+
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                                data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                                data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                ): (
+                    <div className="ratio d-flex justify-content-center">
+                        <img
+                            src="https://placehold.net/400x600.png"
+                            alt={book.title}
+                            className="object-fit-contain p-4"
+                        />
+                    </div>
+                )}
+
+            </div>
+        </>
+    )
+}
--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -2,7 +2,8 @@ import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {getAuthorName, getBook} from "../services/api.js";
 import Description from "../components/BookDescription.jsx";
-import BookButton from "../components/BookButton.jsx";
+import BookButton from "../components/BookDropdown.jsx";
+import BookCoverCarousel from "../components/BookCoverCarousel.jsx";
 
 export default function Book() {
 
@@ -39,20 +40,15 @@ export default function Book() {
     if (error) return <h1>Failed to load book</h1>;
     if (!book) return <h1>No book found</h1>;
 
-    let cover_image;
-    (book.covers) ? cover_image = `https://covers.openlibrary.org/b/id/${book.covers[0]}-L.jpg` : cover_image = "https://placehold.net/400x600.png";
+    // let cover_image;
+    // (book.covers) ? cover_image = `https://covers.openlibrary.org/b/id/${book.covers[0]}-L.jpg` : cover_image = "https://placehold.net/400x600.png";
 
     return (
         <>
             <div className="row g-5">
+
                 <div className="col-6">
-                    <div className="bg-light ratio ratio-1x1 d-flex justify-content-center rounded">
-                        <img
-                            src={cover_image}
-                            className="object-fit-contain p-4"
-                            alt={book.title}
-                        />
-                    </div>
+                    <BookCoverCarousel book={book} />
                 </div>
 
                 <div className="col-6 d-flex flex-column row-gap-3">
@@ -72,21 +68,12 @@ export default function Book() {
                         </p>
                     </div>
 
-                    <BookButton book={book} />
+                    <BookButton book={book}/>
 
                     <Description content={book.description}/>
 
                 </div>
             </div>
-            {/*<hr className="hr my-4"/>*/}
-            {/*<div>*/}
-            {/*    <h2>Overview</h2>*/}
-            {/*    {book.subjects.map((subject, index) => (*/}
-            {/*        <div key={index}>*/}
-            {/*            {subject}*/}
-            {/*        </div>*/}
-            {/*    ))}*/}
-            {/*</div>*/}
         </>
     );
 }
