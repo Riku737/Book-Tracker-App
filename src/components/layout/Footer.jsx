cfcc6e01a089dd408d67cef10eec60555bf5f5aa
Riki <rmcal024@uottawa.ca>
2026-07-11 16:09:58 -0400

    Footer - footer tags

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,56 +4,56 @@ function Footer() {
 
     return(
         <>
-        <div className="bg-light py-5">
-            <div className="container">
+            <footer className="bg-light py-5">
+                <div className="container">
 
-                <div className="row">
+                    <div className="row">
 
-                    <div className="col-lg-3 col-6">
-                        <h6>BookBook</h6>
-                        <p>A clean, simple catalog where you can organize every book in your reading life. It's your
-                            bookshelf.</p>
+                        <div className="col-lg-3 col-6">
+                            <h6>BookBook</h6>
+                            <p>A clean, simple catalog where you can organize every book in your reading life. It's your
+                                bookshelf.</p>
+                        </div>
+
+                        <div className="col-lg-3 col-6">
+                            <h6>Bookshelf</h6>
+                            <ul className="list-unstyled d-flex flex-column gap-2">
+                                <li><Link to="/bookshelf/want_to_read">Want to Read</Link></li>
+                                <li><Link to="/bookshelf/reading">Currently Reading</Link></li>
+                                <li><Link to="/bookshelf/read">Read</Link></li>
+                                <li><Link to="/bookshelf/dnf">Did Not Finish</Link></li>
+                            </ul>
+                        </div>
+
+                        <div className="col-lg-3 col-6">
+                            <h6>Trending</h6>
+                            <ul className="list-unstyled d-flex flex-column gap-2">
+                                <li>Text</li>
+                                <li>Text</li>
+                                <li>Text</li>
+                                <li>Text</li>
+                            </ul>
+                        </div>
+
+                        <div className="col-lg-3 col-6">
+                            <h6>Subject</h6>
+                            <ul className="list-unstyled d-flex flex-column gap-2">
+                                <li>Text</li>
+                                <li>Text</li>
+                                <li>Text</li>
+                                <li>Text</li>
+                            </ul>
+                        </div>
+
                     </div>
 
-                    <div className="col-lg-3 col-6">
-                        <h6>Bookshelf</h6>
-                        <ul className="list-unstyled d-flex flex-column gap-2">
-                            <li><Link to="/bookshelf/want_to_read">Want to Read</Link></li>
-                            <li><Link to="/bookshelf/reading">Currently Reading</Link></li>
-                            <li><Link to="/bookshelf/read">Read</Link></li>
-                            <li><Link to="/bookshelf/dnf">Did Not Finish</Link></li>
-                        </ul>
-                    </div>
+                    <hr/>
 
-                    <div className="col-lg-3 col-6">
-                        <h6>Trending</h6>
-                        <ul className="list-unstyled d-flex flex-column gap-2">
-                            <li>Text</li>
-                            <li>Text</li>
-                            <li>Text</li>
-                            <li>Text</li>
-                        </ul>
-                    </div>
-
-                    <div className="col-lg-3 col-6">
-                        <h6>Subject</h6>
-                        <ul className="list-unstyled d-flex flex-column gap-2">
-                            <li>Text</li>
-                            <li>Text</li>
-                            <li>Text</li>
-                            <li>Text</li>
-                        </ul>
-                    </div>
+                    <p>This website is a personal, non-commercial project. All book metadata and cover images are sourced from the <a href="https://openlibrary.org/" target="_blank">Open Library API</a>, and all trademarks belong to their respective owners.</p>
+                    <p>This site stores data locally in your browser using IndexedDB. No information is shared or uploaded.</p>
 
                 </div>
-
-                <hr/>
-
-                <p>This website is a personal, non-commercial project. All book metadata and cover images are sourced from the <a href="https://openlibrary.org/" target="_blank">Open Library API</a>, and all trademarks belong to their respective owners.</p>
-                <p>This site stores data locally in your browser using IndexedDB. No information is shared or uploaded.</p>
-
-            </div>
-        </div>
+            </footer>
         </>
     );
 
